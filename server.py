1055a1064a29b77046f3249e5eecede3609a7381
iKarans <68602829+iKarans@users.noreply.github.com>
2021-12-01 14:48:58 +0000

    finished server code i think

--- server.py
+++ server.py
@@ -7,6 +7,28 @@ PORT = 9090
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 
-server.listen()
+server.listen(2)
 
 clients = []
+
+def broadcast(move):
+    for client in clients:
+        client.send(move)
+
+def handle(client):
+    while True:
+        try:
+            move = client.recv(1024)
+            broadcast(move)
+        except:
+            clients.remove(client)
+            client.close()
+            break
+
+
+def receive():
+    client, address = server.accept()
+    print(f"client with address: {address} has connected")
+    clients.append(client)
+    thread = threading.Thread(target=handle, args=(client, ))
+    thread.start()
